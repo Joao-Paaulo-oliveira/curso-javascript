ecf597aec4a424469149968b0ad116b2c279fdad
João Paulo <joao.ppaulo92@hotmail.com>
2025-06-18 20:04:57 -0300

    funcoes return

---
+++ funcoesreturn.js
@@ -0,0 +1,8 @@
+
+function gerarNome(nome, sobrenome, idade){
+    return 'Nome: ' + nome + sobrenome + 'Idade: ' + idade
+}
+
+
+const nomeGerado = gerarNome('Samuel', ' Roque ', 12)
+console.log(nomeGerado)
