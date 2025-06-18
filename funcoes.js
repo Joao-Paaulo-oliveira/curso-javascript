71e918616718dfd333034f1af6881281f865de5f
João Paulo <joao.ppaulo92@hotmail.com>
2025-06-18 19:49:48 -0300

    addfuncoes

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+
+function gerarNome() {
+    // Declara variáveis com nome, sobrenome e idade
+    const nome = 'João Paulo'
+    const sobrenome = ' Oliveira, '  // Contém uma vírgula e espaço já embutidos
+    const idade = 33 
+
+    // Imprime o nome completo e a idade
+    console.log('Nome: ', nome + sobrenome, 'Idade: ', idade) 
+}
+
+gerarNome()
