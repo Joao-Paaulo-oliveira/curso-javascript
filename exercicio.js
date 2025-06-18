cb3b01bee453cc3e40027902880b8a4a365d216b
João Paulo <joao.ppaulo92@hotmail.com>
2025-06-17 22:03:15 -0300

    exercicioestruturarepeticao

---
+++ exercicio.js
@@ -0,0 +1,18 @@
+//Exercicio 1 - Estrutura de repetição 
+
+//Percorrer uma list de nomes
+//Eduardo, Maria, Fermando, Joao e José 
+//Numero da execução começando em 1
+//Nome que está sendo executado 
+
+
+const nome = ['Eduardo', 'Maria', 'Fernando', 'João', 'José']
+
+nome.forEach((nomes, indice) => {
+    console.log('Execução: ', indice)
+    console.log('Nome: ' + nomes)
+    console.log('--------------')
+});
+
+
+
