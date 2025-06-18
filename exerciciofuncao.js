3123f855b829b4c0c98ed5a5c623de290ea3b746
João Paulo <joao.ppaulo92@hotmail.com>
2025-06-18 20:16:43 -0300

    exercicio funcao

---
+++ exerciciofuncao.js
@@ -0,0 +1,13 @@
+
+/* Exercicio - Função
+Criar uma funaçao 
+Que soma 2 numeros e retorna esse numero somado 
+Deve ser enviado por parametro esses dois numeros. 
+*/
+
+function soma  (num1,  num2) {
+    return 'A soma dos dois numeros é: ' + (num1 + num2)
+}
+
+const resultado = soma(20, 45)
+console.log(resultado)
